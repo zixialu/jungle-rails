79afd805470bf2cb4f85b40e1a6f997b9bcbe945
Zixia Lu <zixialu@gmail.com>
2019-01-17 12:44:04 -0500

    Implement login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
