0ad84dbae1bd71ff1e35d12f553e9225cfdf5935
Zixia Lu <zixialu@gmail.com>
2019-01-18 12:48:58 -0500

    Create validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,8 @@
 class Rating < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :product_id, presence:true
+  validates :user_id, validates:true
+  validates :rating, validates:true
 end
