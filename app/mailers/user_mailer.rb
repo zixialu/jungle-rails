b3502310947aab94f6092bbad91a44683e9d8b5f
Zixia Lu <zixialu@gmail.com>
2019-01-17 20:13:06 -0500

    Fix email formatting ad passing params

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def order_receipt_email(user, order)
     @user = user
     @order = order
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Order receipt ##{@order.id}')
+    subject_line = "Order receipt ##{@order.id}"
+    mail(to: @user.email, subject: subject_line)
   end
 end
