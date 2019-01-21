790d5459d7cb2dd2700e5e4f989f5bb22ff4af85
Zixia Lu <zixialu@gmail.com>
2019-01-21 14:35:42 -0500

    Implement creating a new review from the form

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,11 +1,27 @@
 class RatingsController < ApplicationController
 
-  def create
+  def new
     @product = Product.find(params[:product_id])
-    @rating = @product.ratings.create(rating_params)
+    @rating = @product.ratings.new
+  end
 
-    if @rating.save
-      redirect to "/products/#{params[:product_id]}"
+  def create
+    if current_user then
+      @product = Product.find(params[:product_id])
+      @rating = @product.ratings.create(
+        product_id: params[:product_id],
+        user_id: current_user.id,
+        rating: rating_params[:rating],
+        description: rating_params[:description]
+      )
+
+      if @rating.save
+        # redirect to "/products/#{params[:product_id]}"
+        redirect_to :back
+      end
+    else
+      # User needs to log in to leave a review
+      redirect_to '/login'
     end
   end
 
